11c20e35dfeffb257a8cf92a0396449e6ec1ac26
Anton Solomonov <weny@bk.ru>
2020-03-02 23:53:34 +0700

    Комменты и ридми

--- alfafront/src/App.vue
+++ alfafront/src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="app">
-    <el-card class="box-card" style="margin-top: 60px;">
+    <el-card class="box-card">
       <div slot="header" class="clearfix">
         <span>Роли</span>
       </div>
-      <el-checkbox-group v-model="userRole" @change="updateUserRole" style="text-align: left;">
+      <el-checkbox-group class="checks" v-model="userRole" @change="updateUserRole">
         <el-checkbox
           v-for="r in role"
           :label="r.id"
@@ -14,12 +14,12 @@
         >{{ r.name }}</el-checkbox>
       </el-checkbox-group>
     </el-card>
-    <el-button-group style="margin-top: 20px;">
+    <el-button-group class="btns">
       <router-link v-for="p in avaibleReport" :key="p.id" :to="{ name: 'Report-'+p.id }">
-        <el-button :type="p.id == currentReportId ? 'primary' : ''" style="margin: 6px;">{{ p.name }}</el-button>
+        <el-button class="tabButton" :type="p.id == currentReportId ? 'primary' : ''">{{ p.name }}</el-button>
       </router-link>
     </el-button-group>
-    <router-view v-if="userRole.length > 0" :key="$route.fullPath" style="margin-top: 20px;" />
+    <router-view class="report" v-if="userRole.length > 0" :key="$route.fullPath" />
   </div>
 </template>
 
@@ -36,9 +36,11 @@ export default {
     };
   },
   computed: {
+    // Вычисляет массив доступных отчетов согласно ролям
     avaibleReport() {
       return this.report.filter(p => this.userRole.includes(p.id));
     },
+    // Вычисляет ID открытого в настоящий момент отчета
     currentReportId() {
       if (this.$route.matched.length == 1) {
         const route = this.$route.matched[0];
@@ -48,8 +50,12 @@ export default {
     }
   },
   methods: {
+    // Отправка ролей на сохранение
     updateUserRole() {
       this.$axios.put("/api/UserRole", this.userRole);
+
+      // Если сейчас открыт отчет, на который
+      // права забрали - закрыть отчет
       if (
         this.userRole.length > 0 &&
         this.currentReportId &&
@@ -60,9 +66,11 @@ export default {
     }
   },
   created() {
+    // Загрузка списка возможных отчетов
     this.$axios.get("/api/Report").then(res => {
       this.report = res.data;
 
+      // Добавление отчетов в роутер
       res.data.map(p => {
         this.$router.addRoutes([
           {
@@ -74,9 +82,13 @@ export default {
         ]);
       });
     });
+
+    // Загрузка возможных ролей
     this.$axios.get("/api/Role").then(res => {
       this.role = res.data;
     });
+
+    // Загрузка ролей пользователя
     this.$axios.get("/api/UserRole").then(res => {
       this.userRole = res.data;
     });
@@ -104,5 +116,20 @@ body {
   color: #2c3e50;
   width: 600px;
   justify-self: center;
+}
+#app .box-card {
+  margin-top: 60px;
+}
+.box-card .checks {
+  text-align: left;
+}
+#app .btns {
+  margin-top: 20px;
+}
+#app .btns .tabButton {
+  margin: 6px;
+}
+#app .report {
+  margin-top: 20px;
 }
 </style>
