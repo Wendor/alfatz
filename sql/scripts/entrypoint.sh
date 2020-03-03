e91b45b1ea9f023ba0a4987e80850dcbeaf7e123
Anton Solomonov <weny@bk.ru>
2020-03-03 09:45:55 +0700

    Entrypoint refactoring

--- sql/scripts/entrypoint.sh
+++ sql/scripts/entrypoint.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-TIMEOUT=20s
+TIMEOUT=2s
 
 echo importing data will start in $TIMEOUT...
 sleep $TIMEOUT
 
-exist=`/opt/mssql-tools/bin/sqlcmd -S 0.0.0.0 -U sa -P $SA_PASSWORD -Q "SET NOCOUNT ON select COUNT(*) from sys.databases where name = 'TestData'" -h -1 | awk '{print $1}'`
-if [ "$exist" = "0" ]; then
-    echo "Database not found. Creating..."
-    /opt/mssql-tools/bin/sqlcmd -S 0.0.0.0 -U sa -P $SA_PASSWORD -i ./sql/create_database.sql
-    /opt/mssql-tools/bin/sqlcmd -S 0.0.0.0 -U sa -P $SA_PASSWORD -i ./sql/init.sql
-    /opt/mssql-tools/bin/sqlcmd -S 0.0.0.0 -U sa -P $SA_PASSWORD -i ./sql/step1.sql
-    /opt/mssql-tools/bin/sqlcmd -S 0.0.0.0 -U sa -P $SA_PASSWORD -i ./sql/step2.sql
-    echo "Database created."
-else
-    echo "Database found. Skip creating."
-fi
+exist=0
+
+while [ "$exist" = "0" ]
+do
+    exist=`/opt/mssql-tools/bin/sqlcmd -S 0.0.0.0 -U sa -P $SA_PASSWORD -Q "SET NOCOUNT ON select COUNT(*) from sys.databases where name = 'TestData'" -h -1 | awk '{print $1}'`
+    if [ "$exist" = "0" ]; then
+        echo "Database not found. Creating..."
+        /opt/mssql-tools/bin/sqlcmd -S 0.0.0.0 -U sa -P $SA_PASSWORD -i ./sql/create_database.sql
+        /opt/mssql-tools/bin/sqlcmd -S 0.0.0.0 -U sa -P $SA_PASSWORD -i ./sql/init.sql
+        /opt/mssql-tools/bin/sqlcmd -S 0.0.0.0 -U sa -P $SA_PASSWORD -i ./sql/step1.sql
+        /opt/mssql-tools/bin/sqlcmd -S 0.0.0.0 -U sa -P $SA_PASSWORD -i ./sql/step2.sql
+        echo "Database created."
+    fi
+    sleep 1
+done
