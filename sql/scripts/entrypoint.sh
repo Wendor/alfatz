f28be04aaaab1c3c0cdd6ae5164422f81bf516fb
Anton Solomonov <weny@bk.ru>
2020-03-03 11:18:24 +0700

    fix entrypoint

--- sql/scripts/entrypoint.sh
+++ sql/scripts/entrypoint.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-TIMEOUT=2s
+TIMEOUT=20s
 
 echo importing data will start in $TIMEOUT...
 sleep $TIMEOUT
 
 exist=0
 
-while [ "$exist" = "0" ]
+while [ "$exist" != "1" ]
 do
     exist=`/opt/mssql-tools/bin/sqlcmd -S 0.0.0.0 -U sa -P $SA_PASSWORD -Q "SET NOCOUNT ON select COUNT(*) from sys.databases where name = 'TestData'" -h -1 | awk '{print $1}'`
-    if [ "$exist" = "0" ]; then
+    if [ "$exist" != "1" ]; then
         echo "Database not found. Creating..."
         /opt/mssql-tools/bin/sqlcmd -S 0.0.0.0 -U sa -P $SA_PASSWORD -i ./sql/create.sql
         /opt/mssql-tools/bin/sqlcmd -S 0.0.0.0 -U sa -P $SA_PASSWORD -i ./sql/init.sql
@@ -19,5 +19,5 @@ do
         /opt/mssql-tools/bin/sqlcmd -S 0.0.0.0 -U sa -P $SA_PASSWORD -i ./sql/step3.sql
         echo "Database created."
     fi
-    sleep 1
+    sleep 5
 done
